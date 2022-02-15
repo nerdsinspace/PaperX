0a1bbf0fe835c825e8a3c15f11a0be2dc1e71311
mattcc <matt.fr1kin@gmail.com>
2022-02-14 19:53:17 -0600

    Format kts files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,66 +1,66 @@
 import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
 
 plugins {
-    java
-    id("com.github.johnrengelman.shadow") version "7.1.1"
-    id("io.papermc.paperweight.patcher") version "1.3.4"
+  java
+  id("com.github.johnrengelman.shadow") version "7.1.1"
+  id("io.papermc.paperweight.patcher") version "1.3.4"
 }
 
 repositories {
-    mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
-    }
+  mavenCentral()
+  maven("https://papermc.io/repo/repository/maven-public/") {
+    content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+  }
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.1")
-    decompiler("net.minecraftforge:forgeflower:1.5.498.22")
-    paperclip("io.papermc:paperclip:3.0.2")
+  remapper("net.fabricmc:tiny-remapper:0.8.1")
+  decompiler("net.minecraftforge:forgeflower:1.5.498.22")
+  paperclip("io.papermc:paperclip:3.0.2")
 }
 
 allprojects {
-    apply(plugin = "java")
-    apply(plugin = "maven-publish")
+  apply(plugin = "java")
+  apply(plugin = "maven-publish")
 
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
-        }
+  java {
+    toolchain {
+      languageVersion.set(JavaLanguageVersion.of(17))
     }
+  }
 }
 
 subprojects {
-    tasks.withType<JavaCompile> {
-        options.encoding = Charsets.UTF_8.name()
-        options.release.set(17)
-    }
-    tasks.withType<Javadoc> {
-        options.encoding = Charsets.UTF_8.name()
-    }
-    tasks.withType<ProcessResources> {
-        filteringCharset = Charsets.UTF_8.name()
-    }
+  tasks.withType<JavaCompile> {
+    options.encoding = Charsets.UTF_8.name()
+    options.release.set(17)
+  }
+  tasks.withType<Javadoc> {
+    options.encoding = Charsets.UTF_8.name()
+  }
+  tasks.withType<ProcessResources> {
+    filteringCharset = Charsets.UTF_8.name()
+  }
 
-    repositories {
-        mavenCentral()
-        maven("https://papermc.io/repo/repository/maven-public/")
-    }
+  repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+  }
 }
 
 paperweight {
-    serverProject.set(project(":PaperX-Server"))
+  serverProject.set(project(":PaperX-Server"))
 
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+  remapRepo.set("https://maven.fabricmc.net/")
+  decompileRepo.set("https://files.minecraftforge.net/maven/")
 
-    usePaperUpstream(providers.gradleProperty("paperRef")) {
-        withPaperPatcher {
-            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-            apiOutputDir.set(layout.projectDirectory.dir("PaperX-API"))
+  usePaperUpstream(providers.gradleProperty("paperRef")) {
+    withPaperPatcher {
+      apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+      apiOutputDir.set(layout.projectDirectory.dir("PaperX-API"))
 
-            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
-            serverOutputDir.set(layout.projectDirectory.dir("PaperX-Server"))
-        }
+      serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+      serverOutputDir.set(layout.projectDirectory.dir("PaperX-Server"))
     }
+  }
 }
